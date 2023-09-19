fca4d8df6c2da70a6e8089a6ade7988e29a955fb
Алекс <Alexandr_enid@mail.ru>
2023-09-19 19:14:00 +0500

    Добавьте файлы проекта.

---
+++ ColorDialogWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorDialogWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowsFormsHost host = new WindowsFormsHost();
+            
+            var colorChoice = new System.Windows.Forms.Button();
+            colorChoice.MouseClick += ColorChoice_MouseClick;
+            host.Child = colorChoice;
+            this.myPanel.Children.Add(host);
+        }
+
+        private void ColorChoice_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            var button = (System.Windows.Forms.Button)sender;
+            ColorDialog colorDialog = new ColorDialog();
+            var dr = colorDialog.ShowDialog();
+            var color = colorDialog.Color;
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                button.BackColor = color;
+            }
+        }
+    }
+}
